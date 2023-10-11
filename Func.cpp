e111779834f286795b7bf5a576e7fbee0987c15a
Max Goryunov <maximthehedgehog@yandex.ru>
2023-10-11 11:30:09 +0300

    add type conversion

--- Func.cpp
+++ Func.cpp
@@ -6,8 +6,8 @@ using fun = std::function<T>;
 Func Func::fromLibrary(HINSTANCE const& library) {
 	return Func(
 		((string(*)(void))GetProcAddress(library, "name"))(),
-		((Precedence(*)(void))GetProcAddress(library, "precedence"))(),
-		((Associativity(*)(void))GetProcAddress(library, "associativity"))(),
+		(Precedence)((int(*)(void))GetProcAddress(library, "precedence"))(),
+		(Associativity)((bool(*)(void))GetProcAddress(library, "associativity"))(),
 		((int(*)(void))GetProcAddress(library, "arity"))(),
 		fun<double(double,double)>((double(*)(double, double))GetProcAddress(library, "apply"))
 	);
