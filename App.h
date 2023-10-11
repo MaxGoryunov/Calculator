450ff76d5341a993d1cb133310de272e4f9d3e3c
Max Goryunov <maximthehedgehog@yandex.ru>
2023-10-11 17:46:39 +0300

    #7 add App class

--- App.h
+++ App.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include "Expression.h"
+#include "DllCatalogue.h"
+#include "Funcs.h"
+
 class App {
 private:
+	Expression& expression;
+	Funcs& funcs;
+public:
+	App(Expression& expression, DllCatalogue& libraries, Funcs& funcs) : 
+		expression(expression), funcs(funcs) {
+		libraries.loadFuncsFromDll(this->funcs);
+	}
 
+	void calculate(string& input);
+
+	App() = default;
+	App(App const& other) = default;
+	App(App&& other) = default;
+	App& operator = (App const& other) = default;
+	App& operator = (App&& other) = default;
+	~App() = default;
 };
